f7f7400ef985173194213e0643a37a0a04caf86d
HuskySatLab <cubesat@uw.edu>
2018-01-02 16:15:42 -0800

    Core:  Updated CRCs

--- COSMOS/flatsat/procedures/LoopToggle.rb
+++ COSMOS/flatsat/procedures/LoopToggle.rb
@@ -1,5 +1,4 @@
 loop do
-  
   cmd("EPS_DIST DOMSWITCH with PD_COM1 TOGGLE")
   cmd("EPS_DIST DOMSWITCH with PD_COM2 TOGGLE")
   cmd("EPS_DIST DOMSWITCH with PD_RAHS TOGGLE")
@@ -8,8 +7,4 @@ loop do
   cmd("EPS_DIST DOMSWITCH with PD_WHEELS TOGGLE")
   cmd("EPS_DIST DOMSWITCH with PD_EPS TOGGLE")
   cmd("EPS_DIST DOMSWITCH with PD_PPT TOGGLE")
-  
-  
-
-
 end
