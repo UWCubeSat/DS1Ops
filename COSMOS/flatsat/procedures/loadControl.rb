27afb1ebaf78cd9d1eeb84223aa163888d5aa11c
Langley DeWitt <langleydewitt@gmail.com>
2018-06-11 14:32:00 -0700

    Added isOn command to digital load

--- COSMOS/flatsat/procedures/loadControl.rb
+++ COSMOS/flatsat/procedures/loadControl.rb
@@ -1,55 +1,71 @@
 def turnOn()
-	begin
-		cmd("DIGITAL_LOAD", "TURN_ON")
-	rescue Exception =>e
-		puts "WARNING. COULD NOT TURN ON INPUT"
-	end
+  begin
+    cmd("DIGITAL_LOAD", "TURN_ON")
+  rescue Exception =>e
+    puts "WARNING. COULD NOT TURN ON INPUT"
+  end
+end
+
+def isOn()
+  begin
+    cmd("DIGITAL_LOAD", "GET_INPUT_STATE")
+    
+    state= tlm("DIGITAL_LOAD STATUS STATE")
+   
+    if state == 1
+      return true
+    else
+      return false
+    end
+  rescue Exception =>e
+    puts "WARNING. COULD NOT GET INPUT STATE"
+  end
 end
 
 def turnOff()
-	begin
-		cmd("DIGITAL_LOAD", "TURN_OFF")
-	rescue Exception =>e
-		puts "WARNING. COULD NOT TURN OFF INPUT"
-	end
+  begin
+    cmd("DIGITAL_LOAD", "TURN_OFF")
+  rescue Exception =>e
+    puts "WARNING. COULD NOT TURN OFF INPUT"
+  end
 end
 
 def setConstantCurrentMode()
-	begin
-		cmd("DIGITAL_LOAD", "SET_CONSTANT_CURRENT_MODE")
-	rescue Exception =>e
-		puts "WARNING. COULD NOT SET CONSTANT CURRENT MODE"
-	end
+  begin
+    cmd("DIGITAL_LOAD", "SET_CONSTANT_CURRENT_MODE")
+  rescue Exception =>e
+    puts "WARNING. COULD NOT SET CONSTANT CURRENT MODE"
+  end
 end
 
 def setConstantVoltageMode()
-	begin
-		cmd("DIGITAL_LOAD", "SET_CONSTANT_VOLTAGE_MODE")
-	rescue Exception =>e
-		puts "WARNING. COULD NOT SET CONSTANT VOLTAGE MODE"
-	end
+  begin
+    cmd("DIGITAL_LOAD", "SET_CONSTANT_VOLTAGE_MODE")
+  rescue Exception =>e
+    puts "WARNING. COULD NOT SET CONSTANT VOLTAGE MODE"
+  end
 end
 
 def setConstantPowerMode()
-	begin
-		cmd("DIGITAL_LOAD", "SET_CONSTANT_POWER_MODE")
-	rescue Exception =>e
-		puts "WARNING. COULD NOT SET CONSTANT POWER MODE"
-	end
+  begin
+    cmd("DIGITAL_LOAD", "SET_CONSTANT_POWER_MODE")
+  rescue Exception =>e
+    puts "WARNING. COULD NOT SET CONSTANT POWER MODE"
+  end
 end
 
 def setConstantResistanceMode()
-	begin
-		cmd("DIGITAL_LOAD", "SET_CONSTANT_RESISTANCE_MODE")
-	rescue Exception =>e
-		puts "WARNING. COULD NOT SET CONSTANT RSISTANCE MODE"
-	end
+  begin
+    cmd("DIGITAL_LOAD", "SET_CONSTANT_RESISTANCE_MODE")
+  rescue Exception =>e
+    puts "WARNING. COULD NOT SET CONSTANT RSISTANCE MODE"
+  end
 end
 
 def setCurrentLevel(i)
-	begin
-		cmd("DIGITAL_LOAD", "SET_CURRENT", "current"=>i)
-	rescue Exception =>e
-		puts "WARNING. COULD NOT SET CURRENT LEVEL"
-	end
+  begin
+    cmd("DIGITAL_LOAD", "SET_CURRENT", "current"=>i)
+  rescue Exception =>e
+    puts "WARNING. COULD NOT SET CURRENT LEVEL"
+  end
 end
