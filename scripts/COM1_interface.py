da7ded1d208614b3b6a9f91965331ce65492a1d1
HuskySatLab <cubesat@uw.edu>
2018-07-27 09:58:20 -0700

    changes to the output of the COM1 interface script to allow for automatically launching Putty

--- scripts/COM1_interface.py
+++ scripts/COM1_interface.py
@@ -6,13 +6,13 @@ import threading
 softwarePort = 256
 
 def main():
+	print("BootloaderNotFound")
 	threads = []
 	for i in range(256):
 		t = threading.Thread(target=tryBootOnPort, args=(i,))
 		threads.append(t)
 		t.start()
 		t.join()
-	print("Execution complete")
 
 def tryBootOnPort(portNum):
 	portIsBootloader = False
@@ -20,7 +20,7 @@ def tryBootOnPort(portNum):
 		bootloader = serial.Serial('COM' + str(portNum), timeout=1)
 		if (isBootloader(bootloader)):
 			bootloader.write(b"a")
-			print('Successfully exited bootloader on COM' + str(portNum))
+			#print('Successfully exited bootloader on COM' + str(portNum))
 			portIsBootloader = True
 	except:
 		return
@@ -29,9 +29,13 @@ def tryBootOnPort(portNum):
 		try:
 			software = serial.Serial('COM' + str(portNum + 1), timeout=1)
 			software.write(b'ignore umbilical\r\n')
+			print("ignore umbilical")
 			software.write(b'health mode\r\n')
+			print("health mode")
 			software.write(b'transponder disable\r\n')
-			print("Software initialized on COM" + str(portNum + 1))
+			print("transponder disable")
+			#print("Software initialized on COM" + str(portNum + 1))
+			print("COM" + str(portNum+1))
 			return
 		except:
 			pass
