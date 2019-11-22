2d04a83977c56fd9113a8d4597862680f663fc86
Nathan Wacker <nathan.wacker@live.com>
2019-11-22 10:40:31 -0800

    improved the wodblock protocol to be less lossy asumming the data arive in time order

--- COSMOS/flatsat/lib/wodblockprotocol.rb
+++ COSMOS/flatsat/lib/wodblockprotocol.rb
@@ -12,39 +12,38 @@ require 'cosmos/config/config_parser'
 require 'thread'
 
 module Cosmos
-  # Base class for all COSMOS protocols which defines a framework which must be
-  # implemented by a subclass.
   class Wodblockprotocol < Protocol
-    @@packet_times = {}
-    @@gnd_class_dict = {0 => "gnd_none", 6 => "gnd_health",5 => "gnd_realtime",7 => "gnd_wod",1 => "camera"}
+
+    def initialize(allow_empty_data = nil)
+      super()
+      @last_wod_uptime = -1.0/0
+      @last_wod_reset  = -1.0/0
+      @last_wod_data   = :STOP
+    end
+
     def read_data(data)
       data = super(data)
       if data != :STOP
-        #puts("+++++++++++++++++++++++++++++"
-        reset = data[4..5].unpack("n")[0]
-        uptime =  data[6..9].unpack("n")[0]
-        #mask the whole id, grab just those important 29 bits
         id = (data[24..27].unpack("N")[0] & ((2**30)-1))
-        gnd_class = @@gnd_class_dict[(id & (2**22 + 2**21 + 2**20)) >> 20]
-        #if (id & (2**22 + 2**21 + 2**20)) >> 20 == 6
-          #puts(id,gnd_class)
-        #end
-        #puts("+++++++++++++++++++++++++++++")
-
-        if @@packet_times.has_key?(id)
-          if (reset >= @@packet_times[id][0] and uptime > @@packet_times[id][1])
-            @@packet_times[id] = [reset,uptime]
-            return data
+        gnd_class = (id & (2**22 + 2**21 + 2**20)) >> 20
+        
+        if gnd_class == 7  # this is WOD
+          reset = data[4..5].unpack("n")[0]
+          uptime =  data[6..9].unpack("N")[0]
+          if uptime < @last_wod_uptime && reset == @last_wod_reset
+            # this packet is older, so pass the last one
+            new_data = data
+            data = @last_wod_data
+            @last_wod_data = new_data
           else
-            return :STOP
+            @last_wod_data = data
+            data = :STOP
           end
-        else
-          @@packet_times[id] = [reset,uptime]
-          return data
+          @last_wod_uptime = uptime
+          @last_wod_reset  = reset
         end
-      else
-        return data
       end
+      return data
     end
   end
 end
